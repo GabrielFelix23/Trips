9503fc9e8faf9d48d075ab38110938a9e5d347fc
GabrielFelix23 <srgfelix123@gmail.com>
2021-03-06 19:18:59 -0300

    Configurando Api

--- trips/src/Pages/Home/index.js
+++ trips/src/Pages/Home/index.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import api from '../../Services/api'
 
 export default function Home() {
+  const [trips, setTrips] = useState([])
+
+  useEffect(() => {
+    async function loadAPI(){
+      const response = await api.get('/trips')
+      setTrips(response.data)
+
+      console.log(response.data)
+    }
+
+    loadAPI()
+  },[])
+
   return (
-    <h1>Home</h1>
+    <div>
+
+    </div>
   )
 }
